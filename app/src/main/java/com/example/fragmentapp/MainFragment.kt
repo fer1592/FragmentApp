f5c06a05e067f653e2ccd2069dd7c93530a79ff1
Fernando perez <fj.perezosa@gmail.com>
2022-01-16 19:06:39 -0300

    feat: added safe args to navigation, added contraint layout to fragments, fix: removed binding var from activities

--- app/src/main/java/com/example/fragmentapp/MainFragment.kt
+++ app/src/main/java/com/example/fragmentapp/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.fragmentapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +16,7 @@ class MainFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater, R.layout.fragment_main, container, false)
 
         binding.btnFragment.setOnClickListener { view: View ->
-            Navigation.findNavController(view).navigate(R.id.myFragment)
+            Navigation.findNavController(view).navigate(MainFragmentDirections.actionMainFragment2ToMyFragment2())
         }
 
         binding.btnActivity.setOnClickListener {
